d781b2542030b8ea111582bf408bc919d6ef887c
Phạm văn sơn <bontukyhpkt@gmail.com>
2024-08-30 10:39:10 +0700

    son

--- slidebars.bat
+++ slidebars.bat
@@ -4,7 +4,7 @@ echo OKAY! configing slidebars %1 ...
 setlocal
 set "rootDir=resources/views/administrator/components"
 
-call :FindReplace "/*step_1*/" "@can('%1-list')<li class='nav-item'><a href='{{route('administrator.%1.index')}}' class='nav-link'><svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='002424' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='featherfeather-boxlink-icon'><path d='M2116V8a22000-1-1.73l-7-4a22000-20l-74A2200038v8a2200011.73l74a2200020l7-4A220002116z'></path><polyline points='3.276.961212.0120.736.96'></polyline><line x1='12' y1='22.08' x2='12' y2='12'></line></svg><span class='link-title'>%1</span></a></li>@endcan/**/" slidebars.blade.php
+call :FindReplace "/*step_1*/" "@can('%1-list')<li class='nav-item'><a href='{{route('administrator.%1.index')}}' class='nav-link'><svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='002424' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='featherfeather-boxlink-icon'><path d='M2116V8a22000-1-1.73l-7-4a22000-20l-74A2200038v8a2200011.73l74a2200020l7-4A220002116z'></path><polyline points='3.276.961212.0120.736.96'></polyline><line x1='12' y1='22.08' x2='12' y2='12'></line></svg><span class='link-title'>%1</span></a></li>@endcan /*step_1*/" slidebars.blade.php
 
 exit /b 
 
